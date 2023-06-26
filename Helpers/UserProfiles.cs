ed67c5929cd9f986d42661a2d6cff1125cbcd7f2
Ajdin Pipo <pipo.m.ajdin@gmail.com>
2023-06-26 14:52:40 +0200

    updated profiles

--- Helpers/UserProfiles.cs
+++ Helpers/UserProfiles.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace VehicleRental.API.Transforms
+namespace VehicleRental.API.Helpers
 {
     public class UserTransforms : Profile
     {
--- Helpers/VehicleProfiles.cs
+++ Helpers/VehicleProfiles.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 
-namespace VehicleRental.API.Transforms
+namespace VehicleRental.API.Helpers
 {
     public class VehicleTransforms : Profile
     {
